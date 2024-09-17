49c6109c0b7a446ba153321f859d5bf9658f2304
Fernando Paz <ferpaz64@gmail.com>
2024-09-16 19:26:18 -0600

    Creado el esqueleto de la App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MapScreenComponent } from './maps/screens/map-screen/map-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'map',
+    component: MapScreenComponent,
+    title: 'Map',
+  },
+  {
+    path: '**',
+    redirectTo: 'map'
+  }
+];
---
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-screen.component.html',
+  styles: ``
+})
+export class MapScreenComponent {
+
+}
