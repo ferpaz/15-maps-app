4eda0ae2b1b45d2e30b5040c3137954b378c7bfa
Fernando Paz <ferpaz64@gmail.com>
2024-09-17 17:19:30 -0600

    Se creo un componente para el logo de la App y para el botón de navegar a la ubicación actual del usuario

---
+++ src/app/maps/components/index.ts
@@ -0,0 +1,4 @@
+export * from './app-logo/app-logo.component';
+export * from './btn-my-location/btn-my-location.component';
+export * from './loading/loading.component';
+export * from './map-view/map-view.component';
--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
+
+import { AppLogoComponent, BtnMyLocationComponent, LoadingComponent, MapViewComponent } from '../../components';
 import { PlacesService } from '../../services';
-import { LoadingComponent } from "../../components/loading/loading.component";
-import { MapViewComponent } from "../../components/map-view/map-view.component";
+
 
 @Component({
   selector: 'app-map-screen',
   standalone: true,
-  imports: [LoadingComponent, MapViewComponent],
+  imports: [LoadingComponent, MapViewComponent, AppLogoComponent, BtnMyLocationComponent],
   templateUrl: './map-screen.component.html',
   styles: ``
 })
