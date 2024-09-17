96b58b33dcb20d776faaf8bbc06f7c02b95bb1ff
Fernando Paz <ferpaz64@gmail.com>
2024-09-17 16:18:47 -0600

    Se agrego Mapbox como proveedor de mapa y ya se muestra en la vista principal de la App

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import Mapboxgl from 'mapbox-gl';
+
+import { environment } from '../environments/environment';
+
+Mapboxgl.accessToken = environment.mapbox.accessToken;
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ],
 };
--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,18 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild, viewChild } from '@angular/core';
+import { Map, Popup, Marker } from 'mapbox-gl';
+
 import { PlacesService } from '../../services';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-map-view',
   standalone: true,
   imports: [],
   templateUrl: './map-view.component.html',
-  styles: ``
+  styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit {
-  ngOnInit(): void {
-    this.placesService.getUserLocation();
-  }
+export class MapViewComponent implements AfterViewInit {
 
+  @ViewChild('mapDiv') mapDivElement!: ElementRef;
   private placesService = inject(PlacesService);
 
+  map: Map | undefined;
+
+  ngAfterViewInit(): void {
+    if (!this.placesService.userLocation) throw new Error('User location not available');
+
+    this.map = new Map({
+      container: this.mapDivElement.nativeElement,
+      style: environment.mapbox.style,
+      center: this.placesService.userLocation,
+      zoom: 16,
+    });
+
+    const popup = new Popup()
+      .setHTML(`
+        <h6>Aquí estoy</h6>
+        <span>Mi posición actual en el mundo</span>
+      `);
+
+    new Marker({ color: '#d00', })
+      .setLngLat(this.placesService.userLocation)
+      .setPopup(popup)
+      .addTo(this.map);
+  }
 }
