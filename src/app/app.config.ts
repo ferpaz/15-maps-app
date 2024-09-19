a2cebc0b3d13bc191e3e948e4e185363c8392ebb
Fernando Paz <ferpaz64@gmail.com>
2024-09-19 09:24:47 -0600

    Limpieza de warnings y code smells

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import Mapboxgl from 'mapbox-gl';
 
 import { environment } from '../environments/environment';
-import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 Mapboxgl.accessToken = environment.mapbox.accessToken;
 
